998f80cce2129e4f0d74b8c29da63bc81ed2bdd3
Steffeni Bunsick <steffenibunsick@gmail.com>
2019-01-15 12:04:13 -0600

    cat

--- bash.js
+++ bash.js
@@ -1,14 +1,19 @@
 process.stdout.write('prompt > ');
 const pwdReq = require('./pwd');
 const lsReq = require('./ls');
+const catReq = require('./cat');
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  switch (cmd) {
+  let fileName = cmd.split(' ')[1];
+  switch (cmd.split(' ')[0]) {
     case 'pwd':
       pwdReq();
       break;
     case 'ls':
       lsReq();
+      break;
+    case 'cat':
+      catReq(fileName);
       break;
     default:
       process.stdout.write('You typed: ' + cmd);
---
+++ cat.js
@@ -0,0 +1,11 @@
+module.exports = fileName => {
+  const fs = require('fs');
+  fs.readFile(`./${fileName}`, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      process.stdout.write(data);
+      process.stdout.write('\nprompt > ');
+    }
+  });
+};
